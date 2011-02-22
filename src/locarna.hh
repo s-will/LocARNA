1d60be9a291fc75bf6e91c50c8c028a227e78c1e
Sebastian Will <swill@csail.mit.edu>
2011-02-22 10:29:09 -0500

    Add missing locarna.hh header

---
+++ src/locarna.hh
@@ -0,0 +1,19 @@
+#ifndef LOCARNA_HH
+#define LOCARNA_HH
+
+// include all headers of the locarna library
+
+#include <LocARNA/sequence.hh>
+#include <LocARNA/basepairs.hh>
+#include <LocARNA/alignment.hh>
+#include <LocARNA/aligner.hh>
+#include <LocARNA/aligner_p.hh>
+#include <LocARNA/evaluator.hh>
+#include <LocARNA/rna_data.hh>
+#include <LocARNA/arc_matches.hh>
+#include <LocARNA/match_probs.hh>
+#include <LocARNA/ribosum.hh>
+#include <LocARNA/anchor_constraints.hh>
+#include <LocARNA/trace_controller.hh>
+
+#endif // LOCARNA_HH
