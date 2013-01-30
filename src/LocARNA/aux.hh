38f74d2cefcfb197a8c6b8368d1404f580ae16ce
Sebastian Will <swill@csail.mit.edu>
2013-01-30 17:35:23 +0100

    Cleanup class StopWatch

--- src/LocARNA/aux.hh
+++ src/LocARNA/aux.hh
@@ -6,10 +6,9 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <tr1/unordered_map>
 #include <assert.h>
 
-#include <sys/time.h> // for gettimeofday
+#include "stopwatch.hh"
 
 //!
 //! auxilliary types and global constants for use in locarna
@@ -400,117 +399,6 @@ namespace LocARNA {
     //! this is useful to distinguish index type from other types that are defined as unsigned int
     typedef type_wrapper<size_t> index_t;
     
-
-
-    
-    /**
-     * @brief control a set of named stop watch like timers
-     */
-    class StopWatch {
-    private:
-	struct timer_t {
-	    bool running; //!<whether the timer is running
-	    double last_start; //!< last start time
-	    double total; //!< total accumulated time
-	    size_t cycles; //!<number of start/stop cycles
-	    
-	    timer_t(): running(false), last_start(0.0), total(0.0), cycles(0) {}
-	};
-
-	//! type of map to store named timers
-	typedef std::tr1::unordered_map<std::string,timer_t> map_t;
-	
-	map_t timers;
-	
-	bool print_on_exit;
-
-    public:
-	
-	StopWatch(bool print_on_exit=true);
-	~StopWatch();
-	
-	
-	void
-	set_print_on_exit(bool print_on_exit);
-	
-	/** 
-	 * @brief start a named timer
-	 * 
-	 * @param name timer name
-	 * 
-	 * @return success
-	 */
-	bool
-	start(const std::string &name);
-	
-	/** 
-	 * @brief stop a named timer
-	 * 
-	 * @param name timer name
-	 * 
-	 * @return success
-	 */
-	bool
-	stop(const std::string &name);
-
-	/** 
-	 * @brief test whether named timer is running
-	 * 
-	 * @param name timer name
-	 * 
-	 * @return running?
-	 */
-	bool
-	is_running(const std::string &name) const;
-
-	/** 
-	 * @brief current total time of a named timer
-	 * 
-	 * @param name timer name
-	 * 
-	 * @return time (if running add time since start)
-	 */
-	double
-	current_total(const std::string &name) const;
-	
-	/** 
-	 * @brief current start/stop cycles of a named timer
-	 * 
-	 * @param name timer name
-	 * 
-	 * @return cycles (including started cycle if running)
-	 */
-	size_t current_cycles(const std::string &name) const;
-	
-	/** 
-	 * @brief print information for one timer
-	 * 
-	 * @param out output stream
-	 * @param name 
-	 *
-	 * @note determine current running time for running timers
-	 *
-	 * @return output stream
-	 */
-	std::ostream &
-	print_info(std::ostream &out,const std::string &name) const;
-
-	/** 
-	 * @brief print information for all timers
-	 * 
-	 * @param out output stream
-	 * 
-	 * @return output stream
-	 * @todo implement
-	 */
-	std::ostream &
-	print_info(std::ostream &out) const;
-
-	
-    private:
-	double current_time () const;
-    };
-
     //! global StopWatch object
     extern StopWatch stopwatch;
 }
