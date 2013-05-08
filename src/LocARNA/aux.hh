47bb25b899647d3ea7194a0ea79fc80347e59bd2
Sebastian Will <swill@csail.mit.edu>
2013-05-08 19:39:29 +0200

    Add tuples.hh header

--- src/LocARNA/aux.hh
+++ src/LocARNA/aux.hh
@@ -71,49 +71,6 @@ namespace LocARNA {
     };
  
 
-    //! @brief Represents a 3-tuple
-    //!
-    //! triple stores three values first, second, third.
-    //! extension of std::pair to 3-tuple
-    template<class T1,class T2,class T3>
-    class triple: public std::pair<T1,T2> {
-    public:
-	T3 third; //!< third value
-	
-	/** 
-	 * Construct from three values 
-	 * 
-	 * @param x1 value 1
-	 * @param x2 value 2
-	 * @param x3 value 3
-	 * 
-	 */
-	triple(const T1 &x1,const T2 &x2,const T3 &x3): std::pair<T1,T2>(x1,x2),third(x3) {
-	}
-    };
-    
-    //! @brief Represents a 4-tuple
-    //!
-    //! quadruple stores four values first, second, third, fourth.
-    //! extension of triple to 4-tuple
-    template<class T1,class T2,class T3,class T4>
-    class quadruple: public triple<T1,T2,T3> {
-    public:
-	T4 fourth; //!< fourth value
-	
-	/** 
-	 * \brief Construct from four values 
-	 * 
-	 * @param x1 value 1
-	 * @param x2 value 2
-	 * @param x3 value 3
-	 * @param x4 value 4
-	 * 
-	 */
-	quadruple(const T1 &x1,const T2 &x2,const T3 &x3,const T4 &x4): triple<T1,T2,T3>(x1,x2,x3),fourth(x4) {
-	}
-    };
-
 
     // ------------------------------------------------------------
     // transformation of strings
