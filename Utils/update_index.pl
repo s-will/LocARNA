4282dedc4c0e1ecbeef28b0df3f614ffe37fd577
Sebastian Will <swill@csail.mit.edu>
2015-09-17 18:39:39 +0200

    Add local mode to release scripts

--- Utils/update_index.pl
+++ Utils/update_index.pl
@@ -3,6 +3,8 @@
 ## update index.html to include list release; move currently latest
 ## release to list of old releases
 
+## don't change if "new" release is already the latest release in index.html
+
 $RELEASE=shift @ARGV;
 if ( ! defined($RELEASE) ) {
     print STDERR "USAGE: update_index.pl <release>\n";
@@ -14,10 +16,17 @@ my $latest_release="";
 my $older_releases="";
 
 while (<>) {
-    if (/^<!--!LATEST RELEASE/) {
+    if (/<!--!LATEST RELEASE\s+(\S+)/) {
+        if ($1 eq "$RELEASE") {
+            ## RELEASE is already the latest one; don't make any changes
+            ## echo rest of input to stdout and exit
+            print $_;
+            while(<>) {print;}
+            exit(0);
+        }
 	while (<>) {
-	    if (/^<!--!END LATEST RELEASE/) {
-		print "<!--!LATEST RELEASE -- don't remove this line -- don't edit until END-->
+	    if (/<!--!END LATEST RELEASE/) {
+		print "<!--!LATEST RELEASE $RELEASE-- don't remove this line -- don't edit until END-->
 <b>Latest Release: <a href=\"Releases/locarna-$RELEASE.tar.gz\"
 	onClick=\"_gaq.push(['_trackEvent', 'Download', 'LocARNA', '$RELEASE']);\"
 	>LocARNA $RELEASE</a></b>
@@ -32,9 +41,9 @@ while (<>) {
 	$latest_release=~s/^\s*<b>Latest Release: //;
 	$latest_release=~s/<\b>\s*$//;
 	
-    } elsif (/^<!--!OLDER RELEASES/) {
+    } elsif (/<!--!OLDER RELEASES/) {
 	while (<>) {
-	    if (/^<!--!END OLDER RELEASES/) {
+	    if (/<!--!END OLDER RELEASES/) {
 		chomp $older_releases;
 		print "<!--!OLDER RELEASES --- don't remove this line-->
 <li>
