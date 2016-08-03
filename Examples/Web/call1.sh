a73fa4492b0e396cbd005c43fba9b2f9299ba5f9
Sebastian Will <swill@csail.mit.edu>
2016-08-03 09:23:45 +0200

    Add test of web examples

---
+++ Examples/Web/call1.sh
@@ -0,0 +1,1 @@
+mlocarna example1.fa --consistency-transformation --struct-weight=200 --use-ribosum=true --max-diff-am=30 --match=50 --max-diff=40 --min-prob=0.01 --tree-min-prob=0.01 --rnafold-temperature=37.0 --noLP --plfold-span=150 --indel=-350 --indel-opening=-500 --ignore-constraints --probabilistic --mismatch=0 --write-structure
---
+++ Examples/Web/call4.sh
@@ -0,0 +1,1 @@
+mlocarna example4.fa --struct-weight=200 --use-ribosum=true --max-diff-am=30 --match=50 --max-diff=60 --min-prob=5.0E-4 --tree-min-prob=0.005 --rnafold-temperature=37.0 --noLP --plfold-span=150 --indel=-350 --indel-opening=-500 --sequ-local=false --mismatch=0 --write-structure
