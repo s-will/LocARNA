da1e7619cab323d8bbbafe23df3260fe98be8c75
Sebastian Will <swill@csail.mit.edu>
2016-11-06 21:21:49 +0100

    Update to VRNA 2.3.0 and fix bug in configure script   * Fix configure bug: correctly recognize C11 features of ViennaRNA   * Require ViennaRNA 2.3.0 (after adapting to renamed VRNA_FC macros)

--- src/LocARNA/mcc_matrices.cc
+++ src/LocARNA/mcc_matrices.cc
@@ -37,7 +37,7 @@ namespace LocARNA {
     McC_matrices_t::McC_matrices_t(vrna_fold_compound_t *vc)
 	:McC_matrices_base(vc)
     {	
-        assert( vc->type == VRNA_VC_TYPE_SINGLE );
+        assert( vc->type == VRNA_FC_TYPE_SINGLE );
     }
 
     McC_matrices_t::~McC_matrices_t() {
@@ -47,7 +47,7 @@ namespace LocARNA {
     McC_ali_matrices_t::McC_ali_matrices_t(vrna_fold_compound_t *vc)
 	:McC_matrices_base(vc)
     {
-        assert( vc->type == VRNA_VC_TYPE_ALIGNMENT );
+        assert( vc->type == VRNA_FC_TYPE_COMPARATIVE );
     }
     
     McC_ali_matrices_t::~McC_ali_matrices_t() {
