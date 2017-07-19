5e726badb69502ee638da03475709262cf5fea52
Sebastian Will <swill@csail.mit.edu>
2017-07-19 14:02:12 +0200

    Add python-like enumerate iterator; fix zip; add unit tests

--- src/LocARNA/zip.hh
+++ src/LocARNA/zip.hh
@@ -40,50 +40,57 @@ namespace LocARNA {
             iterator(iterator1_t c1, iterator2_t c2) : iparent_t(c1, c2) {}
 
             auto operator*() {
-                return value_pair_t(*iparent_t::first, *iparent_t::second);
+                return value_pair_t(*this->first, *this->second);
             }
 
             auto operator-> () const {
-                auto x = std::make_unique<value_pair_t>(*iparent_t::first,
-                                                        *iparent_t::second);
+                auto x = std::make_unique<value_pair_t>(*this->first,
+                                                        *this->second);
                 return x;
             }
 
             auto &operator++() {
-                iparent_t::first++;
-                iparent_t::second++;
+                ++(this->first);
+                ++(this->second);
                 return *this;
             }
 
             auto &
             operator+=(int x) {
-                iparent_t::first += x;
-                iparent_t::second += x;
+                this->first += x;
+                this->second += x;
                 return *this;
             }
 
             auto &
             operator-=(int x) {
-                iparent_t::first -= x;
-                iparent_t::second -= x;
+                this->first -= x;
+                this->second -= x;
                 return *this;
             }
 
             auto
             operator+(int x) const {
-                return iterator(iparent_t::first + x, iparent_t::second + x);
+                return iterator(this->first + x, this->second + x);
             }
 
             auto
             operator-(int x) const {
-                return iterator(iparent_t::first - x, iparent_t::second - x);
+                return iterator(this->first - x, this->second - x);
             }
 
             bool
             operator==(const iterator &x) const {
-                return static_cast<iparent_t>(x).first == iparent_t::first ||
-                    static_cast<iparent_t>(x).second == iparent_t::second;
+                return static_cast<iparent_t>(x).first == this->first ||
+                    static_cast<iparent_t>(x).second == this->second;
             }
+
+            bool
+            operator!=(const iterator &x) const {
+                return static_cast<iparent_t>(x).first != this->first &&
+                    static_cast<iparent_t>(x).second != this->second;
+            }
+
         };
 
         using parent_t = std::pair<T1 &, T2 &>;
@@ -91,20 +98,121 @@ namespace LocARNA {
 
         auto
         begin() {
-            return iterator(parent_t::first.begin(), parent_t::second.begin());
+            return iterator(this->first.begin(), this->second.begin());
         }
 
         auto
         end() {
-            return iterator(parent_t::first.end(), parent_t::second.end());
+            return iterator(this->first.end(), this->second.end());
         }
     };
+
+    template <typename T = size_t>
+    class ConstIterableSequence {
+    public:
+        using value_type = T;
+
+        ConstIterableSequence(value_type start)
+            : start_(start){};
+
+        class const_iterator {
+        public:
+            const_iterator(const value_type &idx) : idx_(idx) {}
+
+            const auto &
+            operator *() const {
+                return idx_;
+            }
+
+            const auto *
+            operator ->() const {
+                return &idx_;
+            }
+
+            auto &operator++() {
+                idx_++;
+                return *this;
+            }
+
+            auto &
+            operator+=(int x) {
+                idx_ += x;
+                return *this;
+            }
+
+            auto &
+            operator-=(int x) {
+                idx_ -= x;
+                return *this;
+            }
+
+            auto
+            operator+(int x) const {
+                return const_iterator(idx_ + x);
+            }
+
+            auto
+            operator-(int x) const {
+                return const_iterator(idx_ - x);
+            }
+
+            bool
+            operator==(const const_iterator &) const {
+                return false;
+            }
+
+            bool
+            operator!=(const const_iterator &) const {
+                return true;
+            }
+
+        private:
+            value_type idx_;
+        };
+
+        using iterator = const_iterator;
+
+        auto
+        begin() const {
+            return const_iterator(start_);
+        }
+        auto
+        end() const {
+            return const_iterator(0);
+        }
+        auto
+        cbegin() const {
+            return begin();
+        }
+        auto
+        cend() const {
+            return end();
+        }
+    private:
+        value_type start_;
+    };
+
+    template <class T>
+    class Enumerate : public Zip<const ConstIterableSequence<>,T> {
+    public:
+        Enumerate(T &c) : Zip<const ConstIterableSequence<>,T>(cis_, c), cis_(0) {}
+
+    private:
+        ConstIterableSequence<> cis_;
+    };
+
+    template <class T>
+    auto
+    enumerate(T &c) {
+        return Enumerate<T>(c);
+    }
 
     template <class T1, class T2>
     auto
     zip(T1 &c1, T2 &c2) {
         return Zip<T1, T2>(c1, c2);
     }
+
 }
 
 #endif // LOCARNA_ZIP_HH
