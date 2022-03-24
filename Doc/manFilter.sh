0764338449331d749184669b117960122ba910e8
Sebastian Will <swill@csail.mit.edu>
2022-03-24 17:52:43 +0100

    Update doxygen documentation
    * use doxygen awesome style
    * use REAMDE.md as main page
    * include ChangeLog.md
    * include man pages from perl scripts and C++ programs
        - we generate markdown from the available help
        - rely on pod2markdown conversion, in case of perl scripts
        - rely on pandoc for converting the help2man-auto-generated man pages to markdown

---
+++ Doc/manFilter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file="$1"
+
+ext="${file##*.}"
+echo "#" $(basename "$file" ".$ext")
+echo
+
+if [ "$ext" = "pl" ] ; then
+  # if perl file than
+  pod2markdown "$file"
+else
+  # directly convert man page
+  # Note: grep and sed clean artifacts of the current conversion
+
+  pandoc $file --from man --to markdown | grep -vx '\\' | sed 's/**\\-/**-/g'
+fi
